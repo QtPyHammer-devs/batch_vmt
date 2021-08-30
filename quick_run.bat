6d85f7d8b59f70e45042c7fdce211d43c3be1bb0
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2021-08-30 17:01:32 +1000

    nvm it broke

--- quick_run.bat
+++
@@ -1,8 +0,0 @@
-py -3.9 -m venv venv
-venv/scripts/activate
-python -m pip --upgrade pip
-python -m pip install -r requirements.txt
-:: can folders be passed to the CLI script with a drag and drop?
-:: e.g. `python batch_vmt.py %*` ?
-python batch_vmt.py
-deactivate
